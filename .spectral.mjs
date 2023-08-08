20353e931466b7c55c9f7ee5d3d1ea7534906cd3
kurmangazykongratbayev@outlook.com <kurmangazykongratbayev@outlook.com>
2023-08-08 15:01:41 +0600

    Modified 3 files

---
+++ .spectral.mjs
@@ -0,0 +1,2 @@
+import ruleset from "https://stoplight.io/api/v1/projects/cHJqOjIxMzQ2Ng/spectral.js?branch_slug=master&token=df3ed2d7-a15a-4cf2-ba80-e59c59b4cfcf";
+export default { extends: ruleset };
